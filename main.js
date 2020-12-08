6715dee778a6e10623041cc5002b3da61558ebb0
faridt4242 <45771679+faridt4242@users.noreply.github.com>
2020-12-08 15:05:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -159,16 +159,6 @@ const getChallenges = async () => {
 function checkforms() {
   // get all the inputs within the submitted form
   var inputs = document.getElementsByTagName("input");
-  for (var i = 0; i < inputs.length; i++) {
-    // only validate the inputs that have the required attribute
-    if (inputs[i].hasAttribute("required")) {
-      if (inputs[i].value == "") {
-        // found an empty field that is required
-        alert("Please fill all required fields");
-        return false;
-      }
-    }
-  }
   return true;
 }
 
